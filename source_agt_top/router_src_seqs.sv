e6a75162cfa54176b69fd26b0e9e590c7d8a05ac
Krishna Chaitanya <66372002+Krishnachaitanya-422@users.noreply.github.com>
2023-03-21 14:00:01 +0530

    Update router_src_seqs.sv

--- source_agt_top/router_src_seqs.sv
+++ source_agt_top/router_src_seqs.sv
@@ -8,10 +8,6 @@ class router_sbase_seq extends uvm_sequence #(trxn);
 
 	// Standard UVM Methods:
     extern function new(string name ="router_sbase_seq");
-	
-
-
-
 
 endclass
 
@@ -19,3 +15,23 @@ endclass
 function router_sbase_seq::new(string name ="router_sbase_seq");
 	super.new(name);
 endfunction
+
+class router_sbase_seq_c1 extends router_sbase_seq;
+	`uvm_object_utils(router_sbase_seq_c1);
+	
+	extern function new(string name="router_sbase_seq_c1");
+	extern task body;
+	
+endclass
+
+function router_sbase_seq_c1::new(string name="router_sbase_seq_c1");
+	super.new(name);
+endfunction
+
+task router_sbase_seq_c1::body;
+
+	req=trxn::type_id::create("router_sbase_seq");
+	start_item(req);
+	assert(req.randomize with {header[7:2}==10;);
+
+endtask
