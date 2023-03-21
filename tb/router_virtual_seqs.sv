030d36467cd59a18a69f61319d263fb08d733231
Krishna Chaitanya <66372002+Krishnachaitanya-422@users.noreply.github.com>
2023-03-21 14:38:54 +0530

    Update router_virtual_seqs.sv

--- tb/router_virtual_seqs.sv
+++ tb/router_virtual_seqs.sv
@@ -3,15 +3,28 @@ class router_virtual_sequence extends uvm_sequence #(uvm_sequence_item) ;
     // Factory Registration
 	`uvm_component_utils(router_virtual_sequencer)
 
-   // Declare dynamic array of handles for ram_wr_sequencer and ram_rd_sequencer as wr_seqrh[] & rd_seqrh[]
-
+   // Declare dynamic array of handles for router_wr_sequencer and ram_rd_sequencer as wr_seqrh[] & rd_seqrh[]
+	router_dst_sequencer v_seqrh;
 	router_src_sequencer s_seqrh[];
 	router_dst_sequencer d_seqrh[];
 
-    // Declare handle for ram_env_config 
+    // Declare handle for router_env_config 
    	 router_env_config m_cfg;
 	 
-	 
-	// src and dst seq handles
-	// 
+	extern function new(string name="router_virtual_sequence");
+	extern task body;
+	
 endclass
+
+
+function router_virtual_sequence::new(string name="router_virtual_sequence");
+	super.new(name);
+endfunction
+
+task router_virtual_sequence::body;
+
+	
+
+
+
+endtask
